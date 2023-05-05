aed66cdc83ff0c67cb02ad7885d73c6a548b9915
tan29121996 <88268203+tan29121996@users.noreply.github.com>
2023-05-05 20:17:18 +0800

    Update firebase.js

--- renderer/firebase.js
+++ renderer/firebase.js
@@ -9,13 +9,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAUr_3JEEFlab9O8ceXwoQ_S9OIjroVknQ",
-  authDomain: "klick-d6cea.firebaseapp.com",
-  projectId: "klick-d6cea",
-  storageBucket: "klick-d6cea.appspot.com",
-  messagingSenderId: "860270821976",
-  appId: "1:860270821976:web:8797713e45f087b07c002d",
-  measurementId: "G-98FB6S9KB9"
+
 };
 
 // Initialize Firebase
