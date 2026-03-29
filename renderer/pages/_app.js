7e83cbb066efd2c5997060a482d00a355e1621a8
tan29121996 <tan29121996@gmail.com>
2026-03-29 22:01:33 +0800

    Removed Components from app js

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -3,12 +3,12 @@ if (typeof window !== 'undefined' && typeof window.global === 'undefined') {
 }
 
 import '../assets/styles.css';
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...Component} {...pageProps} />
+=      <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
